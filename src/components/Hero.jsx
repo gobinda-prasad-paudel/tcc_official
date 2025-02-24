5a4f267c226393f0205364fa74d31eb8833da9b9
Gobinda Prasad Paudel <gobindapaudelofficial@gmail.com>
2025-02-24 23:38:11 +0545

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,17 +2,32 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center p-4 md:p-8">
+    <div className="flex flex-col md:flex-row items-center justify-center p-2 md:p-6">
       {/* Left Section */}
       <div className="md:w-2/3 w-full text-center md:text-left md:px-4 order-2 md:order-1">
         <h1 className="text-4xl md:text-5xl font-semibold text-white">
           Trinity Computer Council
         </h1>
         <p className="text-lg md:text-xl text-white mt-4">
-          Trinity Computer Council is a student organization that aims to
+          {/*  Trinity Computer Council is a student organization that aims to
           provide a platform for students to showcase their technical skills and
           knowledge. We conduct various workshops, hackathons, and events to
-          help students learn and grow in the field of technology.
+          help students learn and grow in the field of technology. */}
+          Trinity Computer Council is a student-led organization focused on
+          providing a platform for students to develop and showcase their
+          technical skills and knowledge. Our goal is to create an environment
+          that encourages innovation, learning, and collaboration in the
+          ever-evolving field of technology. We organize a variety of events,
+          including workshops, hackathons, and seminars, to help students learn
+          new technologies and improve their problem-solving abilities. These
+          activities offer hands-on experience and foster creativity, providing
+          students with the opportunity to apply theoretical concepts to
+          real-world challenges. By engaging with industry professionals, we
+          also offer exposure to the latest trends and technologies, giving
+          students valuable insights into potential career paths. Whether you’re
+          interested in coding, cybersecurity, AI, or software development, the
+          Trinity Computer Council is a space for you to grow and connect with
+          like-minded individuals.
         </p>
         <a
           href="#projects"
@@ -23,9 +38,9 @@ const Hero = () => {
       </div>
 
       {/* Right Section */}
-      <div className="md:w-1/3 w-full order-1 md:order-2 mt-6 md:mt-0">
+      <div className="md:w-1/3 w-full order-1 md:order-2 mt-4 md:mt-0">
         <img
-          src="./src/assets/club_logo.png"
+          src="https://scontent.fktm17-1.fna.fbcdn.net/v/t39.30808-6/241166429_106503511767148_265667793722597058_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=uz5oH6Eifr0Q7kNvgGuZt9u&_nc_oc=AdgOi_wKZqroJFLNoa14q3A5amjYH3gm3O42nFjB5Gm0c5HGiVONBzvEedc58LaiFLQxYCvFxigy-hoN-dcA0kDM&_nc_zt=23&_nc_ht=scontent.fktm17-1.fna&_nc_gid=ABJGDxWRELwqw1KuzPrthx-&oh=00_AYBEi8yzlOvXQpCIlcQEbj6qgjuYC5H9UGi6XSHYubotnA&oe=67C28034"
           alt="Trinity Computer Council Logo"
           className="w-3/4 md:w-full mx-auto rounded-lg"
         />
