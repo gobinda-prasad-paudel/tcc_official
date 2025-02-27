11769fd57dca25d67e661dd762a52f019ded872c
Gobinda Prasad Paudel <gobindapaudelofficial@gmail.com>
2025-02-27 16:32:05 +0545

    404 page commit

--- src/pages/Pagenotfound.jsx
+++ src/pages/Pagenotfound.jsx
@@ -7,7 +7,7 @@ function Pagenotfound() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto px-4">
+      {/* <div className="container mx-auto px-4">
         <div className="flex items-center flex-col justify-center h-screen">
           <h1 className="text-4xl font-bold text-[#3498db]">
             404 Page Not Found
@@ -23,15 +23,34 @@ function Pagenotfound() {
             </Link>
             <br />
             <Link
-              className="underline text-2xl font-bold text-gray-700 hover:text-gray-900"
-              to="/"
+            className="underline text-2xl font-bold text-gray-700 hover:text-gray-900"
+            to="/"
             >
-              Go back to home
+            Go back to home
             </Link>
-          </div>
-        </div>
+            </div>
+            </div>
+            </div> */}
+
+      <div className="flex flex-col items-center justify-center min-h-screen text-gray-300 ">
+        <h1 className="text-9xl font-bold text-gray-500">404</h1>
+        <h2 className="text-3xl font-semibold mt-4">Not Found</h2>
+        <p className="text-lg mt-2 ">
+          The resource requested could not be found on this server!
+        </p>
+
+        <Link
+          className="text-lg mt-2 text-red-600 hover:text-red-800"
+          to="/developer"
+        >
+          {" "}
+          Contact Developer
+        </Link>
+        <Link className="text-lg mt-2 text-red-600 hover:text-red-800" to="/">
+          {" "}
+          Back To Home
+        </Link>
       </div>
-      <Footer />
     </>
   );
 }
